26581537c7795a5dfcf1c8433235263d77e5242c
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2025-12-17 17:55:46 -0800

    refactor: clean up ModEntry and wire up dependencies

--- ModEntry.cs
+++ ModEntry.cs
@@ -5,22 +5,26 @@ using StardewUI.Framework;
 using StardewValley;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using HotKeyViewer.Infrastructure;
+using HotKeyViewer.Services;
+using HotKeyViewer.Features.Overlay;
 
 namespace HotKeyViewer
 {
     internal sealed class ModEntry : Mod
     {
         private IViewEngine _viewEngine = null!;
-        private IMenuController? _activeMenuController = null;
-        private KeyboardViewModel _viewModel = new();
         private ModConfig _config = null!;
+        private GameState _gameState = null!;
+        private KeybindingService _keybindingService = null!;
+        private KeyboardViewModel _viewModel = null!;
+        private OverlayService _overlayService = null!;
 
         public override void Entry(IModHelper helper)
         {
             _config = helper.ReadConfig<ModConfig>();
 
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
-            helper.Events.Input.ButtonPressed += OnButtonPressed;
         }
 
         private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
@@ -28,7 +32,27 @@ namespace HotKeyViewer
             _viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")
                 ?? throw new InvalidOperationException("StardewUI not found");
 
+            // Initialize dependencies
+            _gameState = new GameState();
+            _keybindingService = new KeybindingService(_gameState);
+            _viewModel = new KeyboardViewModel(_keybindingService);
+
+            // Register the views directory (assets/views)
+            string layoutViewName = $"Mods/{ModManifest.UniqueID}/Views/KeyboardOverlay";
+            _viewEngine.RegisterViews($"Mods/{ModManifest.UniqueID}/Views", "assets/views");
+
+            // Initialize Feature Services
+            _overlayService = new OverlayService(Helper, _viewEngine, _config, _viewModel, layoutViewName);
+
             // Register GMCM
+            RegisterGmcm();
+
+            // Enable hot reloading for development
+            _viewEngine.EnableHotReloadingWithSourceSync();
+        }
+
+        private void RegisterGmcm()
+        {
             var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
             if (configMenu != null)
             {
@@ -45,40 +69,6 @@ namespace HotKeyViewer
                     getValue: () => _config.ToggleKey,
                     setValue: value => _config.ToggleKey = value
                 );
-            }
-
-            // Register the views directory (assets/views)
-            _viewEngine.RegisterViews($"Mods/{ModManifest.UniqueID}/Views", "assets/views");
-
-            // Enable hot reloading for development
-            _viewEngine.EnableHotReloadingWithSourceSync();
-        }
-
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            if (!Context.IsPlayerFree) return;
-
-            if (e.Button == _config.ToggleKey)
-            {
-                ToggleMenu();
-            }
-        }
-
-        private void ToggleMenu()
-        {
-            if (_activeMenuController != null)
-            {
-                _activeMenuController.Close();
-                _activeMenuController = null;
-            }
-            else
-            {
-                _activeMenuController = _viewEngine.CreateMenuControllerFromAsset($"Mods/{ModManifest.UniqueID}/Views/KeyboardOverlay", _viewModel);
-                
-                // Cleanup on close
-                _activeMenuController.Closed += () => _activeMenuController = null;
-                
-                Game1.activeClickableMenu = _activeMenuController.Menu;
             }
         }
     }
